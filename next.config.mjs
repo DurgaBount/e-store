77d217ab84a7b626a6a4d1cd5ac169fa2957ccc3
DurgaBount <durga.prasad@bounteous.com>
2024-01-24 01:56:13 +0530

    added env in the nextjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { domains: ["res.cloudinary.com"] },
+  env: {
+    NEXT_PUBLIC_API_URL:
+      "http://localhost:3000/api/4c8fbf39-cfea-433e-968d-a03f2fe8c364",
+  },
 };
 
 export default nextConfig;
