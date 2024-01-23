77995185c53ad7990c33c31278a7151b8969f92e
DurgaBount <durga.prasad@bounteous.com>
2024-01-24 01:47:11 +0530

    add to cart and checkout implemented

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,27 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/ui/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  console.log("ppp", products);
+
+  const billboard = await getBillboard("e6a25ad5-925e-4a45-b7f6-e831fe879460");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
