9a4c51c168b9e946acdb334e7049231ccbe2a8d1
DurgaBount <durga.prasad@bounteous.com>
2024-01-24 09:35:33 +0530

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   images: { domains: ["res.cloudinary.com"] },
   env: {
     NEXT_PUBLIC_API_URL:
-      "http://localhost:3000/api/4c8fbf39-cfea-433e-968d-a03f2fe8c364",
+      "https://ecommerce-admin-two-rosy.vercel.app/api/4c8fbf39-cfea-433e-968d-a03f2fe8c364",
   },
 };
 
