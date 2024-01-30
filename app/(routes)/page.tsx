d4e8cd89ea243e541401b817da4f2004cac90e83
DurgaBount <durga.prasad@bounteous.com>
2024-01-30 19:05:29 +0530

    fixed config issue

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -3,6 +3,7 @@ import getProducts from "@/actions/get-products";
 import Billboard from "@/components/ui/billboard";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
+import getBillboardsList from "@/actions/get-billboards-list";
 
 export const revalidate = 0;
 
@@ -10,11 +11,14 @@ const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
   console.log("ppp", products);
 
-  const billboard = await getBillboard("e6a25ad5-925e-4a45-b7f6-e831fe879460");
+  const billboards = await getBillboardsList();
+
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard data={billboard} />
+        {billboards.map((billboard) => {
+          return <Billboard key={billboard.id} data={billboard} />;
+        })}
 
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title="Featured Products" items={products} />
--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   images: { domains: ["res.cloudinary.com"] },
   env: {
     NEXT_PUBLIC_API_URL:
-      "https://ecommerce-admin-two-rosy.vercel.app/api/4c8fbf39-cfea-433e-968d-a03f2fe8c364",
+      "https://ecommerce-admin-two-rosy.vercel.app/api/019961d8-eb16-45c8-aa36-5b3070002fa1",
   },
 };
 
